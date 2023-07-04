bec2637a8ff5534fc8f96feda6b9e92ba0e0b2c7
miroshnikoff <taxi83080@gmail.com>
2023-07-04 18:18:11 +0300

    add video.py

---
+++ src/video.py
@@ -0,0 +1,33 @@
+from src.channel import Channel
+
+
+class Video:
+    """Класс для видео ютуб"""
+
+    #Объект для работы с YouTube API
+    youtube = Channel.get_service()
+
+    def __init__(self, video_id):
+        """Экземпляр инициализируется id видео. Дальше все данные будут подтягиваться по API."""
+        self.video_id = video_id
+        self.video = Video.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                 id=video_id
+                                                 ).execute()
+        self.title: str = self.video['items'][0]['snippet']['title']
+        self.url = f"https://www.youtube.com/watch?v={self.video_id}"
+        self.view_count: int = self.video['items'][0]['statistics']['viewCount']
+        self.like_count: int = self.video['items'][0]['statistics']['likeCount']
+
+    def __str__(self):
+        """Возвращает строку с названием видео"""
+        return self.title
+
+
+class PLVideo(Video):
+    """Класс для плейлиста видео ютуб, дочерний от Video"""
+    def __init__(self, video_id, pl_id):
+        """Расширяет функционал класса Video добавлением id плейлиста"""
+        super().__init__(video_id)
+        self.pl_id = pl_id
+
+
