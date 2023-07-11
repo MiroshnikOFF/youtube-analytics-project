02833bbae05cfabcaa8c22e96879d4432af5b3e9
miroshnikoff <taxi83080@gmail.com>
2023-07-11 15:00:15 +0300

    refactor video.py

--- src/video.py
+++ src/video.py
@@ -13,10 +13,18 @@ class Video:
         self.video = Video.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
                                                  id=video_id
                                                  ).execute()
-        self.title: str = self.video['items'][0]['snippet']['title']
-        self.url = f"https://www.youtube.com/watch?v={self.video_id}"
-        self.view_count: int = self.video['items'][0]['statistics']['viewCount']
-        self.like_count: int = self.video['items'][0]['statistics']['likeCount']
+        try:
+            self.title: str = self.video['items'][0]['snippet']['title']
+        except IndexError:
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
+        else:
+            self.title: str = self.video['items'][0]['snippet']['title']
+            self.url = f"https://www.youtube.com/watch?v={self.video_id}"
+            self.view_count: int = self.video['items'][0]['statistics']['viewCount']
+            self.like_count: int = self.video['items'][0]['statistics']['likeCount']
 
     def __str__(self):
         """Возвращает строку с названием видео"""
